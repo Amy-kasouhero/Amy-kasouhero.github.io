a764f4f80482d2fc8a083202d30250e3742220e8
Amy-kasouhero <39583662+Amy-kasouhero@users.noreply.github.com>
2018-05-24 17:02:05 +0900

    Create nicolaffi.js

---
+++ nicolaffi.js
@@ -0,0 +1,19 @@
+        //配列は0から
+      var manga = [ "<img src='manga/manga001.jpg'>",
+                    
+                  ]
+        //配列は0から
+      var picture = [ "<img src=''>"
+                      
+                    ]
+        //配列は0から
+      var diary = ["<iframe src='diary/diary001.html'>"
+
+                  ]
+
+        //関数定義
+      function expression(amy_productions){
+        document.getElementById("amy_works").innerHTML=amy_productions;
+      }
+
+
