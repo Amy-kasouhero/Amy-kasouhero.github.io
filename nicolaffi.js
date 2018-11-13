c37bc78f4a8550e89b1c172c6772389e82083012
Amy-kasouhero <yyilvb.amy.second@gmail.com>
2018-11-13 14:23:15 +0900

    automator1113

--- nicolaffi.js
+++ nicolaffi.js
@@ -1,5 +1,5 @@
         //配列は0から
-      var manga = [ "<img src='manga001b.jpg'>",
+      var mangas = [ "<img src='manga001b.jpg'>",
                     "<img src='manga002a.jpg'>",
                     "<img src='manga003a.jpg'>",
                     "<img src='manga004a.jpg'>",
@@ -30,44 +30,44 @@
                   ]
 
         //配列は0から
-      var diary = [ "<iframe src='diary001.html'>",
-                    "<iframe src='diary002.html'>",
-                    "<iframe src='diary003.html'>",
-                    "<iframe src='diary004.html'>",
-                    "<iframe src='diary005.html'>",
-                    "<iframe src='diary006.html'>",
-                    "<iframe src='diary007.html'>",
-                    "<iframe src='diary008.html'>",
-                    "<iframe src='diary009.html'>",
-                    "<iframe src='diary010.html'>",
-                    "<iframe src='diary011.html'>",
-                    "<iframe src='diary012.html'>",
-                    "<iframe src='diary013.html'>",
-                    "<iframe src='diary014.html'>",
-                    "<iframe src='diary015.html'>"
+      var diaries = [ "<iframe src='diary001.html'>",
+                      "<iframe src='diary002.html'>",
+                      "<iframe src='diary003.html'>",
+                      "<iframe src='diary004.html'>",
+                      "<iframe src='diary005.html'>",
+                      "<iframe src='diary006.html'>",
+                      "<iframe src='diary007.html'>",
+                      "<iframe src='diary008.html'>",
+                      "<iframe src='diary009.html'>",
+                      "<iframe src='diary010.html'>",
+                      "<iframe src='diary011.html'>",
+                      "<iframe src='diary012.html'>",
+                      "<iframe src='diary013.html'>",
+                      "<iframe src='diary014.html'>",
+                      "<iframe src='diary015.html'>"
 
-                  ]
-
-      var drawing = [ "<iframe src='drawing001.html'>",
-                      "<iframe src='drawing002.html'>",
-                      "<iframe src='drawing003.html'>",
-                      "<iframe src='drawing004.html'>",
-                      "<iframe src='drawing005.html'>",
-                      "<iframe src='drawing006.html'>",
-                      "<iframe src='drawing007.html'>",
-                      "<iframe src='drawing008.html'>",
-                      "<iframe src='drawing009.html'>",
-                      "<iframe src='drawing010.html'>",
-                      "<iframe src='drawing011.html'>",
-                      "<iframe src='drawing012.html'>",
-                      "<iframe src='drawing013.html'>",
-                      "<iframe src='drawing014.html'>",
-                      "<iframe src='drawing015.html'>",
-                      "<iframe src='drawing016.html'>"
-      
                     ]
+
+      var drawings = [ "<iframe src='drawing001.html'>",
+                        "<iframe src='drawing002.html'>",
+                        "<iframe src='drawing003.html'>",
+                        "<iframe src='drawing004.html'>",
+                        "<iframe src='drawing005.html'>",
+                        "<iframe src='drawing006.html'>",
+                        "<iframe src='drawing007.html'>",
+                        "<iframe src='drawing008.html'>",
+                        "<iframe src='drawing009.html'>",
+                        "<iframe src='drawing010.html'>",
+                        "<iframe src='drawing011.html'>",
+                        "<iframe src='drawing012.html'>",
+                        "<iframe src='drawing013.html'>",
+                        "<iframe src='drawing014.html'>",
+                        "<iframe src='drawing015.html'>",
+                        "<iframe src='drawing016.html'>"
+      
+                      ]
 
         //関数定義
       function expression(amy_productions){
-        document.getElementById("amy_works").innerHTML=amy_productions;
+        document.getElementById("amy_works").innerHTML = amy_productions;
       }
