b557e541f9bd6bf0d9af3080c0516eca269790a4
Amy-kasouhero <yyilvb.amy.second@gmail.com>
2018-11-14 23:49:11 +0900

    automator1114

--- nicolaffi.js
+++ nicolaffi.js
@@ -1,69 +1,69 @@
         //配列は0から
-      var mangas = [ "<img src='manga001b.jpg'>",
-                    "<img src='manga002a.jpg'>",
-                    "<img src='manga003a.jpg'>",
-                    "<img src='manga004a.jpg'>",
-                    "<img src='manga005a.jpg'>",
-                    "<img src='manga006a.jpg'>",
-                    "<img src='manga007a.jpg'>",
-                    "<img src='manga008a.jpg'>",
-                    "<img src='manga009a.jpg'>",
-                    "<img src='manga010a.jpg'>",
-                    "<img src='manga011a.jpg'>",
-                    "<img src='manga012a.jpg'>",
-                    "<img src='manga013a.jpg'>",
-                    "<img src='manga014a.jpg'>",
-                    "<img src='manga015a.jpg'>",
-                    "<img src='manga016a.jpg'>"
+      var mangas = [ "<img src='comics/manga001b.jpg'>",
+                    "<img src='comics/manga002a.jpg'>",
+                    "<img src='comics/manga003a.jpg'>",
+                    "<img src='comics/manga004a.jpg'>",
+                    "<img src='comics/manga005a.jpg'>",
+                    "<img src='comics/manga006a.jpg'>",
+                    "<img src='comics/manga007a.jpg'>",
+                    "<img src='comics/manga008a.jpg'>",
+                    "<img src='comics/manga009a.jpg'>",
+                    "<img src='comics/manga010a.jpg'>",
+                    "<img src='comics/manga011a.jpg'>",
+                    "<img src='comics/manga012a.jpg'>",
+                    "<img src='comics/manga013a.jpg'>",
+                    "<img src='comics/manga014a.jpg'>",
+                    "<img src='comics/manga015a.jpg'>",
+                    "<img src='comics/manga016a.jpg'>"
                     
                   ]
       var onesyota_manga = [
-                    "<img src='onesyota_manga001a.jpg'>",
-                    "<img src='onesyota_manga002a.jpg'>"
+                    "<img src='comics/onesyota_manga001a.jpg'>",
+                    "<img src='comics/onesyota_manga002a.jpg'>"
                   ]
       var lipcream_manga = [
-                    "<img src='lipcream_manga001a.jpg'>",
-                    "<img src='lipcream_manga002a.jpg'>",
-                    "<img src='lipcream_manga003a.jpg'>",
-                    "<img src='lipcream_manga004a.jpg'>",
-                    "<img src='lipcream_manga005a.jpg'>"
+                    "<img src='comics/lipcream_manga001a.jpg'>",
+                    "<img src='comics/lipcream_manga002a.jpg'>",
+                    "<img src='comics/lipcream_manga003a.jpg'>",
+                    "<img src='comics/lipcream_manga004a.jpg'>",
+                    "<img src='comics/lipcream_manga005a.jpg'>"
                   ]
 
         //配列は0から
-      var diaries = [ "<iframe src='diary001.html'>",
-                      "<iframe src='diary002.html'>",
-                      "<iframe src='diary003.html'>",
-                      "<iframe src='diary004.html'>",
-                      "<iframe src='diary005.html'>",
-                      "<iframe src='diary006.html'>",
-                      "<iframe src='diary007.html'>",
-                      "<iframe src='diary008.html'>",
-                      "<iframe src='diary009.html'>",
-                      "<iframe src='diary010.html'>",
-                      "<iframe src='diary011.html'>",
-                      "<iframe src='diary012.html'>",
-                      "<iframe src='diary013.html'>",
-                      "<iframe src='diary014.html'>",
-                      "<iframe src='diary015.html'>"
+      var diaries = [ "<iframe src='diaries/diary001.html'>",
+                      "<iframe src='diaries/diary002.html'>",
+                      "<iframe src='diaries/diary003.html'>",
+                      "<iframe src='diaries/diary004.html'>",
+                      "<iframe src='diaries/diary005.html'>",
+                      "<iframe src='diaries/diary006.html'>",
+                      "<iframe src='diaries/diary007.html'>",
+                      "<iframe src='diaries/diary008.html'>",
+                      "<iframe src='diaries/diary009.html'>",
+                      "<iframe src='diaries/diary010.html'>",
+                      "<iframe src='diaries/diary011.html'>",
+                      "<iframe src='diaries/diary012.html'>",
+                      "<iframe src='diaries/diary013.html'>",
+                      "<iframe src='diaries/diary014.html'>",
+                      "<iframe src='diaries/diary015.html'>"
 
                     ]
 
-      var drawings = [ "<iframe src='drawing001.html'>",
-                        "<iframe src='drawing002.html'>",
-                        "<iframe src='drawing003.html'>",
-                        "<iframe src='drawing004.html'>",
-                        "<iframe src='drawing005.html'>",
-                        "<iframe src='drawing006.html'>",
-                        "<iframe src='drawing007.html'>",
-                        "<iframe src='drawing008.html'>",
-                        "<iframe src='drawing009.html'>",
-                        "<iframe src='drawing010.html'>",
-                        "<iframe src='drawing011.html'>",
-                        "<iframe src='drawing012.html'>",
-                        "<iframe src='drawing013.html'>",
-                        "<iframe src='drawing014.html'>",
-                        "<iframe src='drawing015.html'>",
-                        "<iframe src='drawing016.html'>"
+      var drawings = [  "<iframe src='drawings/drawing001.html'>",
+                        "<iframe src='drawings/drawing002.html'>",
+                        "<iframe src='drawings/drawing003.html'>",
+                        "<iframe src='drawings/drawing004.html'>",
+                        "<iframe src='drawings/drawing005.html'>",
+                        "<iframe src='drawings/drawing006.html'>",
+                        "<iframe src='drawings/drawing007.html'>",
+                        "<iframe src='drawings/drawing008.html'>",
+                        "<iframe src='drawings/drawing009.html'>",
+                        "<iframe src='drawings/drawing010.html'>",
+                        "<iframe src='drawings/drawing011.html'>",
+                        "<iframe src='drawings/drawing012.html'>",
+                        "<iframe src='drawings/drawing013.html'>",
+                        "<iframe src='drawings/drawing014.html'>",
+                        "<iframe src='drawings/drawing015.html'>",
+                        "<iframe src='drawings/drawing016.html'>"
       
                       ]
 
